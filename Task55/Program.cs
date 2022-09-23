63b28afff2dbad6837be6284a4ffaf9dc18a0a3b
Dmitry Budyakov <dnikbu@yandex.ru>
2022-09-23 23:25:22 +0300

    Task55 - через переменную

---
+++ Task55/Program.cs
@@ -0,0 +1,75 @@
+﻿// Задача 55: 
+// Задайте двумерный массив. Напишите программу,
+// которая заменяет строки на столбцы. В случае, если это
+// невозможно, программа должна вывести сообщение для
+// пользователя.
+
+int GeneratorRandomInt(int minValue, int maxValue)  // генератор Random int в диапазоне [min,max]
+{
+    Random rnd = new Random();
+    return rnd.Next(minValue, maxValue + 1);
+}
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)  // создает массив
+{
+    int[,] matrix = new int[rows, columns];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = GeneratorRandomInt(min, max);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)     // вывод массива в консоль
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        // Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j]} ");
+            else Console.Write($"{matrix[i, j]}");
+        }
+        // Console.WriteLine("]");
+        Console.WriteLine();
+    }
+}
+
+void MatrixRowColSwap(int[,] matrix)    // замена строк на столбцы через переменную
+{
+    int temp = default;
+    int rows = matrix.GetLength(0);
+    int columns = matrix.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = i + 1; j < columns; j++)
+        {
+            temp = matrix[i, j];
+            matrix[i, j] = matrix[j, i];
+            matrix[j, i] = temp;
+        }
+    }
+}
+
+Console.Clear();
+int matrixSizeMin = 3;
+int matrixSizeMax = 4;
+int matrixElemMin = 1;
+int matrixElemMax = 9;
+int matrixRow = GeneratorRandomInt(matrixSizeMin, matrixSizeMax);
+int matrixCol = GeneratorRandomInt(matrixSizeMin, matrixSizeMax);
+int[,] array2D = CreateMatrixRndInt(matrixRow, matrixCol, matrixElemMin, matrixElemMax);
+
+PrintMatrix(array2D);
+Console.WriteLine();
+
+if (array2D.GetLength(0) == array2D.GetLength(1))
+{
+    MatrixRowColSwap(array2D);
+    PrintMatrix(array2D);
+}
+else Console.WriteLine("Это сделать невозможно.");
